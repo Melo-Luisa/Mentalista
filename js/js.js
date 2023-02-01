5424ed72889c175fe26b2136edd602e5ae2d87f6
Luisa_Melo <luhcotmel@gmail.com>
2023-02-01 09:51:48 -0300

    Submit inicial
    Subindo projeto ao github - mentalista

---
+++ js/js.js
@@ -0,0 +1,29 @@
+let numero = parseInt(Math.random() * 1001)
+let chute = document.querySelector('#chute')
+let result = document.querySelector('#result')
+let button = document.querySelector('#button')
+/*while(chute != numero){
+    if(chute == numero){
+        result.innerHTML = 'Acertou'
+    }else if(chute > numero){
+        result.innerHTML ='Errou... o número secreto é menor'
+    }else if(chute < numero){
+        result.innerHTML ='Errou... o número secreto é maior'
+    }
+}*/
+button.addEventListener('click', ()=>{
+    console.log(numero)
+    result.style.padding = '20px'
+    if(chute.value == numero){
+        result.innerHTML = 'Acertou!'
+        result.style.color = 'green'
+    }else if(chute.value > numero){
+        result.innerHTML ='Errou... o número secreto é menor'
+        result.style.color = 'red'
+    }else if(chute.value < numero){
+        result.innerHTML ='Errou... o número secreto é maior'
+        result.style.color = 'yellow'
+    }else if(chute.value == null){
+        result.innerHTML = 'digite algum numero'
+    }
+})
