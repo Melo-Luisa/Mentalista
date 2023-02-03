ef2cd720393321006fc047ae64cb1c05bca52437
Luisa_Melo <luhcotmel@gmail.com>
2023-02-02 22:09:51 -0300

    ajuste

--- js/js.js
+++ js/js.js
@@ -2,15 +2,7 @@ let numero = parseInt(Math.random() * 1001)
 let chute = document.querySelector('#chute')
 let result = document.querySelector('#result')
 let button = document.querySelector('#button')
-/*while(chute != numero){
-    if(chute == numero){
-        result.innerHTML = 'Acertou'
-    }else if(chute > numero){
-        result.innerHTML ='Errou... o número secreto é menor'
-    }else if(chute < numero){
-        result.innerHTML ='Errou... o número secreto é maior'
-    }
-}*/
+
 button.addEventListener('click', ()=>{
 
     result.style.padding = '20px'
